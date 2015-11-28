39e91e57d0453843428ce064f6c47986e3630d9b
Angel Lacret <alacret@gmail.com>
2015-11-28 12:32:53 -0430

    inital node commit

---
+++ h.js
@@ -0,0 +1,28 @@
+var Client = require('hangupsjs');
+//var Q = require('q');
+
+// callback to get promise for creds using stdin. this in turn
+// means the user must fire up their browser and get the
+// requested token.
+var creds = function() {
+  return {
+    auth: Client.authStdin
+  };
+};
+
+var client = new Client();
+
+// set more verbose logging
+client.loglevel('debug');
+
+// receive chat message events
+client.on('chat_message', function(ev) {
+  return console.log(ev);
+});
+
+// connect and post a message.
+// the id is a conversation id.
+client.connect(creds).then(function() {
+    return client.sendchatmessage('UgwgjAkjSbqRJ0ALdsx4AaABAQ',
+    [[0, 'I am a mother fucker nodeJD bot created by alacret']]);
+}).done();
